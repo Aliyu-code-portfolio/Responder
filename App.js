051dab12a9930f179b30c8f45ee19ff5bbe4e6fe
Aliyu <88856370+AllyCodeX@users.noreply.github.com>
2022-07-19 07:31:15 +0100

    Update App.js

--- App.js
+++ App.js
@@ -20,13 +20,13 @@ export default function App() {
     return null;
   }
   const firebaseConfig = {
-    apiKey: 'AIzaSyC5JP7q6d4M21jrcA7R1zh6-NgtFlQMMBI',
-    authDomain: 'laesna-d7c83.firebaseapp.com',
-    projectId: 'laesna-d7c83',
-    storageBucket: 'laesna-d7c83.appspot.com',
-    messagingSenderId: '955485269180',
-    appId: '1:955485269180:web:f66e1be2004d44911ab4b9',
-    measurementId: 'G-23L5DWHGSZ',
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
   };
   if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
